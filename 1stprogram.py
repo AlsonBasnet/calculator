1f573234a38e804c30b6036b72fab3c8ddd6efa1
AlsonBasnet <basnetalson121@gmail.com>
2024-05-16 19:31:50 +0545

    Update and rename README.md to 1stprogram.py

---
+++ 1stprogram.py
@@ -0,0 +1,21 @@
+#calculator
+# This is a calculator capable of performing addition, subtraction, multiplication, division, floor division,exponential and modulous of the given two numbers by the user.
+
+n=int(input("enter the first number"))
+m=int(input("enter the second number"))
+add=n+m
+print("The addition of the two numbers is",add)
+sub=n-m
+print("The subtraction of the two numbers is",sub)
+mut=n*m
+print("The multiplication of the two numbers is",mut)
+div=n/m
+print("The division of the two numbers is",div)
+floor=n//m
+print("The floor division of the two numbers is",floor)
+exp=n**m
+print("The exponential of the two numbers is",exp)
+mod=n%m
+print("The modulous of the two numbers is",mod)
+
+
